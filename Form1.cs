c4979fe0240c652766759fc6b00c9d163d1f9d4d
Aljan Patrick Mendoza <aljanpatrick123@gmail.com>
2021-03-25 15:38:52 +0800

    Simple calculator imititating windows calculator

---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+
+        Double value = 0;
+        String operation = "";
+        bool operation_pressed = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void button_Click(object sender, EventArgs e)
+            {
+            if ((result.Text == "0")||(operation_pressed))
+                result.Clear();
+
+                operation_pressed = false;
+                Button b = (Button)sender;
+                result.Text = result.Text + b.Text;
+            }
+
+        private void clearentry_Click(object sender, EventArgs e)
+        {
+            result.Text = "0";
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            operation = b.Text;
+            value = Double.Parse(result.Text);
+            operation_pressed = true;
+            equation.Text = value + " " + operation;
+
+        }
+
+        private void result_click(object sender, EventArgs e)
+        {
+            equation.Text = "";
+            switch (operation)
+            {
+
+                case "+":
+                    result.Text = (value + Double.Parse(result.Text)).ToString();
+                    break;
+                case "-":
+                    result.Text = (value - Double.Parse(result.Text)).ToString();
+                    break;
+                case "*":
+                    result.Text = (value * Double.Parse(result.Text)).ToString();
+                    break;
+                case "/":
+                    result.Text = (value / Double.Parse(result.Text)).ToString();
+                    break;
+                default:
+                    break;
+            } //end of switch    
+           
+        }
+
+        private void clear_click(object sender, EventArgs e)
+        {
+            result.Clear();
+            value = 0;
+        }
+    }
+    }
